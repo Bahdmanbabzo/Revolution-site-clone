de12a22375c47c2b9d1cb38986a079c1a81a43e8
Oserebameh Augustine, Beckley <beckaustine@outlook.com>
2025-01-02 17:28:11 +0100

    Animated image section.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,11 @@
 import { motion } from "motion/react"
-import { animate, stagger } from "motion"
+import { animate, delay, stagger } from "motion"
 import { useEffect } from "react"
 
 export default function Hero() {
     useEffect(() => {
-        animate(".roll-in", {y: ["100%", "0%"]}, {delay: stagger(0.2), ease:"easeInOut", type:"spring", stiffness:60, damping:10})
+        animate(".roll-in", {y:"0%"}, {delay: stagger(0.2), ease:"easeInOut", type:"spring", stiffness:60, damping:10})
+        animate("img", {scale: [0.5,1], x: "0%"}, {delay: stagger(0.2), ease:"easeInOut", type:"spring", stiffness:60, damping:10})
     },[])
     return(
         <div className="h-screen w-screen grid place-items-center bg-[#E4E3E2]">
@@ -18,8 +19,11 @@ export default function Hero() {
                     <motion.p initial={{y:"100%"}} className="h-full w-full roll-in font-Silkscreen text-6xl font-extrabold ">Lorem.</motion.p>
                 </div>
             </section>
-            <section className="absolute bottom-0 right-0 border-2 border-black flex w-1/3 h-32">
-                <img src="" alt="" className="h-full w-1/4" /><img src="" alt="" className="h-full w-1/4" /><img src="" alt="" className="h-full w-1/4" /><img src="" alt="" className="h-full w-1/4" />
+            <section className="absolute bottom-0 right-0 flex h-20 gap-2 m-5 overflow-hidden">
+                <motion.img initial={{scale:0, x:"100%"}}  className="h-full w-1/3 img-slide rounded-lg" src="/augustus3.png" alt="" />
+                <motion.img initial={{scale:0, x:"100%"}}  className="h-full w-1/3 img-slide rounded-lg" src="/Las-Medulas,-Spain.jpg" alt="" />
+                <motion.img initial={{scale:0, x:"100%"}}  className="h-full w-1/3 img-slide rounded-lg" src="/Vatnajokull-Iceland.jpg" alt="" />
+
             </section>
         </div>
     )
