b9dbf068bdc1d8bea01e9ecf2a01e19e63e3d055
Oserebameh Augustine, Beckley <beckaustine@outlook.com>
2025-01-02 11:33:38 +0100

    Importing new fonts.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,15 +8,18 @@ export default function Hero() {
     },[])
     return(
         <div className="h-screen w-screen grid place-items-center bg-[#E4E3E2]">
-            <section>
-                <div className="overflow-hidden h-max w-max">
+            <section className="flex flex-col justify-center items-center">
+                <div className="overflow-hidden h-max w-max font-Tinos text-6xl">
                     <motion.p initial={{y:"100%"}} className="h-full w-full roll-in">Lorem, ipsum</motion.p>
                 </div>
-                <div className="overflow-hidden h-max w-max">
+                <div className="overflow-hidden h-max w-max font-Tinos text-6xl">
                     <motion.p initial={{y:"100%"}} className="h-full w-full roll-in">Lorem, ipsum dolor.</motion.p>
                 </div> <div className="overflow-hidden h-max w-max">
-                    <motion.p initial={{y:"100%"}} className="h-full w-full roll-in">Lorem.</motion.p>
+                    <motion.p initial={{y:"100%"}} className="h-full w-full roll-in font-Silkscreen text-6xl font-extrabold ">Lorem.</motion.p>
                 </div>
+            </section>
+            <section className="absolute bottom-0 right-0 border-2 border-black flex w-1/3 h-32">
+                <img src="" alt="" className="h-full w-1/4" /><img src="" alt="" className="h-full w-1/4" /><img src="" alt="" className="h-full w-1/4" /><img src="" alt="" className="h-full w-1/4" />
             </section>
         </div>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Tinos: ['Tinos', 'sans-serif'],
+        Silkscreen: ['Silkscreen', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
