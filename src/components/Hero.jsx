beec7f3b7874e39188f286cb51eade18594e8f3b
Oserebameh Augustine, Beckley <beckaustine@outlook.com>
2025-01-02 11:04:59 +0100

    Refactored text reveal animation.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,23 @@
 import { motion } from "motion/react"
+import { animate, stagger } from "motion"
+import { useEffect } from "react"
 
 export default function Hero() {
+    useEffect(() => {
+        animate(".roll-in", {y: ["100%", "0%"]}, {delay: stagger(0.2), ease:"easeInOut", type:"spring", stiffness:43, damping:30})
+    },[])
     return(
         <div className="h-screen w-screen flex bg-[#E4E3E2]">
-            <div className="overflow-hidden h-max w-max">
-                <motion.p
-                    initial={{ y: "100%"}}
-                    animate={{ y: "0%"}}
-                    transition={{ duration: 1}}
-                    className="h-full w-full"
-                >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro?</motion.p>
-            </div>
+            <section>
+                <div className="overflow-hidden h-max w-max">
+                    <motion.p initial={{y:"100%"}} className="h-full w-full roll-in">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro?</motion.p>
+                </div>
+                <div className="overflow-hidden h-max w-max">
+                    <motion.p initial={{y:"100%"}} className="h-full w-full roll-in">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro?</motion.p>
+                </div> <div className="overflow-hidden h-max w-max">
+                    <motion.p initial={{y:"100%"}} className="h-full w-full roll-in">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro?</motion.p>
+                </div>
+            </section>
         </div>
     )
 }
