b99ffded5fe608ac29eb582eac85e6697ab53420
Oserebameh Augustine, Beckley <beckaustine@outlook.com>
2025-01-02 11:10:17 +0100

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,18 +4,18 @@ import { useEffect } from "react"
 
 export default function Hero() {
     useEffect(() => {
-        animate(".roll-in", {y: ["100%", "0%"]}, {delay: stagger(0.2), ease:"easeInOut", type:"spring", stiffness:43, damping:30})
+        animate(".roll-in", {y: ["100%", "0%"]}, {delay: stagger(0.2), ease:"easeInOut", type:"spring", stiffness:60, damping:10})
     },[])
     return(
-        <div className="h-screen w-screen flex bg-[#E4E3E2]">
+        <div className="h-screen w-screen grid place-items-center bg-[#E4E3E2]">
             <section>
                 <div className="overflow-hidden h-max w-max">
-                    <motion.p initial={{y:"100%"}} className="h-full w-full roll-in">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro?</motion.p>
+                    <motion.p initial={{y:"100%"}} className="h-full w-full roll-in">Lorem, ipsum</motion.p>
                 </div>
                 <div className="overflow-hidden h-max w-max">
-                    <motion.p initial={{y:"100%"}} className="h-full w-full roll-in">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro?</motion.p>
+                    <motion.p initial={{y:"100%"}} className="h-full w-full roll-in">Lorem, ipsum dolor.</motion.p>
                 </div> <div className="overflow-hidden h-max w-max">
-                    <motion.p initial={{y:"100%"}} className="h-full w-full roll-in">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro?</motion.p>
+                    <motion.p initial={{y:"100%"}} className="h-full w-full roll-in">Lorem.</motion.p>
                 </div>
             </section>
         </div>
