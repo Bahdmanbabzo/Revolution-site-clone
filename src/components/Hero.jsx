a3600198835b6b9a837b7c541764cb83d6037a8a
Oserebameh Augustine, Beckley <beckaustine@outlook.com>
2025-01-08 17:37:45 +0100

    Ref the image on hover.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,19 @@
 import { motion } from "motion/react"
 import { animate, stagger } from "motion"
-import { useEffect } from "react"
+import { useEffect, useState,useRef } from "react"
 
 export default function Hero() {
+    const [render, setRender] = useState(null);
+    const imageRef = useRef();
     useEffect(() => {
         const sequence = [
             [".roll-in", { y: "0%" }, { delay: stagger(0.2), ease: "easeInOut", type: "spring", stiffness: 60, damping: 10 }],
             ["img", { x:"0%", scale:1}, { delay: stagger(0.2), ease: "easeInOut", type: "spring", stiffness: 20, damping: 10 }]
         ]
         animate(sequence)
-    },[])
+        console.log(render)
+        console.log(imageRef.current.src)
+    },[render])
     return(
         <div className="h-screen w-screen grid place-items-center bg-[#E4E3E2]">
             <section className="flex flex-col justify-center items-center">
@@ -23,9 +27,16 @@ export default function Hero() {
                 </div>
             </section>
             <section className="absolute bottom-0 right-0 flex h-20 gap-2 m-5 overflow-hidden p-2 w-1/4">
-                <motion.img initial={{scale:0, x:"500%"}}  className="h-full w-1/3 img-slide rounded-lg" src="/augustus3.png" alt="" />
-                <motion.img initial={{ x:"500%"}}  className="h-full w-1/3 img-slide rounded-lg" src="/Las-Medulas,-Spain.jpg" alt="" />
-                <motion.img initial={{ x:"500%"}}  className="h-full w-1/3 img-slide rounded-lg" src="/Vatnajokull-Iceland.jpg" alt="" />
+                <motion.img 
+                    onHoverStart={()=> setRender(1)} 
+                    initial={{scale:0, x:"400%"}}  
+                    className="h-full w-1/3 img-slide rounded-lg" 
+                    src="/augustus3.png" 
+                    alt="" 
+                    ref={imageRef}
+                />
+                <motion.img initial={{ x:"400%"}}  className="h-full w-1/3 img-slide rounded-lg" src="/Las-Medulas,-Spain.jpg" alt="" />
+                <motion.img initial={{ x:"400%"}}  className="h-full w-1/3 img-slide rounded-lg" src="/Vatnajokull-Iceland.jpg" alt="" />
 
             </section>
         </div>
